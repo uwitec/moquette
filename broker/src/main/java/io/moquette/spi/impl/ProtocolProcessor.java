7e8787fc384262d3d2319096885fc0d95151f584
Shimon Xin <shimon.xin@gmail.com>
2016-03-03 10:46:41 +0800

    notify client disconnected when client connection lost

--- broker/src/main/java/io/moquette/spi/impl/ProtocolProcessor.java
+++ broker/src/main/java/io/moquette/spi/impl/ProtocolProcessor.java
@@ -612,6 +612,7 @@ public class ProtocolProcessor {
             forwardPublishWill(will, clientID);
             m_willStore.remove(clientID);
         }
+        m_interceptor.notifyClientDisconnected(clientID);
     }
     
     /**
